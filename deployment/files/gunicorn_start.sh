d8484b466cbba8de42898e176fc6d70462b0ba2c
Tamás Hauer <Tamas.Hauer@prodema.ch>
2015-03-26 15:04:59 +0100

    roles..

--- deployment/files/gunicorn_start.sh
+++ deployment/files/gunicorn_start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
  
-NAME="hello_app"                                  # Name of the application
-DJANGODIR=/webapps/hello_django/mysite            # Django project directory
-SOCKFILE=/webapps/hello_django/run/gunicorn.sock  # we will communicte using this unix socket
-USER=hello                                        # the user to run as
-GROUP=webapps                                     # the group to run as
-NUM_WORKERS=1                                     # how many worker processes should Gunicorn spawn
-DJANGO_SETTINGS_MODULE=mysite.settings            # which settings file should Django use
-DJANGO_WSGI_MODULE=mysite.wsgi                    # WSGI module name
+NAME="django_tutorial"                               # Name of the application
+DJANGODIR=/webapps/django-tutorial/mysite            # Django project directory
+SOCKFILE=/webapps/django-tutorial/run/gunicorn.sock  # we will communicte using this unix socket
+USER=webapps                                         # the user to run as
+GROUP=webapps                                        # the group to run as
+NUM_WORKERS=1                                        # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=mysite.settings               # which settings file should Django use
+DJANGO_WSGI_MODULE=mysite.wsgi                       # WSGI module name
  
 echo "Starting $NAME as `whoami`"
  
@@ -21,8 +21,7 @@ export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
  
-# Start your Django Unicorn
-# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+# Start your Django Unicorn - (do not use --daemon under supervisor)
 exec ../bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
   --workers $NUM_WORKERS \
@@ -30,16 +29,3 @@ exec ../bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --bind=unix:$SOCKFILE \
   --log-level=info \
   --log-file=-
-
-
-# CONFIG = {
-#   'mode': 'wsgi',
-#   'working_dir': '/var/www/mysite',
-#   'python': '/usr/bin/python',
-#   'args': (
-#       '--bind=127.0.0.1:8002',
-#       '--workers=16',
-#       '--timeout=60',
-#       'mysite.wsgi:application',
-#   ),
-# }
