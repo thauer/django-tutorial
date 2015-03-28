8c7800be7c219c93828a3bd70e3c690fd223457f
Tamás Hauer <Tamas.Hauer@prodema.ch>
2015-03-28 23:21:19 +0100

    postgresql instead of sqlite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -59,9 +59,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '10.15.20.83',
+        'PORT': '5432',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Internationalization
